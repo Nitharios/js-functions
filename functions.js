53265d7d441f8c7c9a6bf1b4a57f7f96dc9dc34e
Nathan N <Nitharios@users.noreply.github.com>
2017-08-13 21:43:41 -1000

    Created areEqual function

--- functions.js
+++ functions.js
@@ -155,7 +155,14 @@ function isLessThan (a, b) {
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
-
+function areEqual (a, b) {
+	if (a === b) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 /**
  * Returns the smallest value of two numbers.
